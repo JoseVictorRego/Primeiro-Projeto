9a499a94aaf83efbb77df678fa321bdccb70805b
JoseVictorRego <victor.rego@upe.br>
2023-07-23 17:13:16 -0300

    add

--- ChatClient.java
+++ ChatClient.java
@@ -1,3 +1,49 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class ChatClient {
-    
+    private static final String SERVER_ADDRESS = "127.0.0.1"; // ip do serivor mais pode ser o dns.
+    private Socket clientSocket; //o socket do client
+    private Scanner scanner;     // função scanner
+    private BufferedWriter out;
+
+    public ChatClient(){
+        scanner = new Scanner(System.in); //chat do servidor
+    }
+
+    public void start() throws IOException{
+        clientSocket = new Socket(SERVER_ADDRESS, ChatServer.PORT);
+        
+        this.out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream())); //poder enviar um conjunto de string
+        
+        System.out.println("Cliente conectado ao servidor em " + SERVER_ADDRESS + " : " + ChatServer.PORT);
+        messageLoop();
+    }
+
+    private void messageLoop() throws IOException{
+        String msg;
+        do{
+            System.out.print("\nDigite (ou 'sair' para finalizar): ");
+            msg = scanner.nextLine();
+            out.write(msg);
+            out.newLine();
+            out.flush();
+
+        }while(!msg.equalsIgnoreCase("sair"));
+    }
+
+    public static void main(String[] args){
+        try {
+            ChatClient client = new ChatClient(); 
+            client.start(); //iniciar a função principal
+
+        } catch (Exception ex) {
+            System.out.println("Erro ao iniciar cliente: " + ex.getMessage());
+        }
+
+        System.out.println("Cliente finalizado!");
+    }
 }
--- ChatServer.java
+++ ChatServer.java
@@ -1,22 +1,43 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.net.ServerSocket; //importar a função de socket
+import java.net.Socket;
 import java.io.IOException;   //importar a função de condicional
 
 public class ChatServer {
-    private final int PORT = 4000;
-    private ServerSocket serverSocket;
+    public static final int PORT = 4000; /*Porta do servidor*/
+    private ServerSocket serverSocket;   /*socket do servidor*/
+    private BufferedReader in;
 
-    public void start() throws IOException{
-        serverSocket = new ServerSocket(PORT);
+    public void start() throws IOException{    /*Metodo de start das função */ 
+        serverSocket = new ServerSocket(PORT); /*Informar a porta do servidor*/
+        System.out.println("Servidor iniciado na porta: " + PORT);
+        
+        clientLoopconect();                    /*Chamando a função de coneção em loop do criente */
     }
 
-    public static void main(String[] args){
+    private void clientLoopconect() throws IOException{ /*função de coneção em loop do criente*/
+        while(true){
+            Socket clientSocket = serverSocket.accept();
+            System.out.println("\n\n-> Cliente Conectou!!");
+            this.in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream())); //poder ler um conjunto de string
+
+            String msg = in.readLine();
+            System.out.print(clientSocket.getRemoteSocketAddress()+": "+ msg);
+            
+        }
+    }
+
+    public static void main(String[] args){ //Main do servidor
 
         try{
-            ChatServer server = new ChatServer();
-            server.start();
+            ChatServer server = new ChatServer(); 
+            server.start();  //iniciar a função principal
+            
         }catch(IOException ex){
             System.out.println("Erro ao iniciar o servidor"+ex.getMessage());
         }
-        
+
+        System.out.println("\nServidor finalizado\n");
     }
 }
